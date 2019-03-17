ac031201278142a1aa4e9f12ebeb8c5da8209b0a
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2019-03-17 16:15:00 +0100

    amélioration temperature

--- controle_temperature.py
+++ controle_temperature.py
@@ -11,12 +11,12 @@ error_max = False
 time_error_max = 0
 last_day = 0
 temp_ok = False
-second = 5
+second = 10
 temperature = 1
 
 try:
     while True:
-        if second == 5:
+        if second == 10:
             second = 1
             content = functions.read_file("/sys/bus/w1/devices/28-01142f1e02d2/w1_slave")
             temperature = functions.get_temp(content)
@@ -26,14 +26,13 @@ try:
                 continue
 
             if temperature < 23:
-
-                temp_min = temperature * 0.85
-                temp_max = temperature * 1.15
+                temp_min = temperature * 0.95
+                temp_max = temperature * 1.05
 
                 content = functions.read_file("/sys/bus/w1/devices/28-01142f1e02d2/w1_slave")
-                temperature = functions.get_temp(content)
+                temperature2 = functions.get_temp(content)
 
-                if temp_min < temperature < temp_max:
+                if temp_min < temperature2 < temp_max:
                     temp_ok = False
 
                     if error_min is False:
@@ -42,19 +41,18 @@ try:
                     error_min = True
                     time_error_min = time_error_min + 1
 
-                    if time_error_min > 180000 and error_min is True:
+                    if time_error_min > 18000 and error_min is True:
                         time_error_min = 0
                         message = "Temperature - RAPPEL ERREUR - trop froid "+str(temperature)+"°C"
 
             elif temperature > 28:
-
-                temp_min = temperature * 0.85
-                temp_max = temperature * 1.15
+                temp_min = temperature * 0.95
+                temp_max = temperature * 1.05
 
                 content = functions.read_file("/sys/bus/w1/devices/28-01142f1e02d2/w1_slave")
-                temperature = functions.get_temp(content)
+                temperature2 = functions.get_temp(content)
 
-                if temp_min < temperature < temp_max:
+                if temp_min < temperature2 < temp_max:
 
                     temp_ok = False
 
@@ -64,7 +62,7 @@ try:
                     error_max = True
                     time_error_max = time_error_max + 1
 
-                    if time_error_max > 180000 and error_max is True:
+                    if time_error_max > 18000 and error_max is True:
                         time_error_max = 0
                         message = "Temperature - RAPPEL ERREUR - trop chaud "+str(temperature)+"°C"
 
@@ -88,7 +86,7 @@ try:
                 body = "<p style='color:green;'>" + message + "</p>"
                 functions.mail(message, body)
 
-        # on envoie un mail de controle tous les jours  8h
+        # on envoie un mail de controle tous les jours à 8h
         now = datetime.datetime.now().strftime('%H%M')
         day = datetime.datetime.now().strftime('%d')
 
