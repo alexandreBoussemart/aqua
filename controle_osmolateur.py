0dbbe527746e0282cf1e077639f04b570d256575
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2019-03-29 17:34:49 +0100

    debug

--- controle_osmolateur.py
+++ controle_osmolateur.py
@@ -65,6 +65,8 @@ try:
         # on fait la chaine de caractère du statut
         state_current = TO_HIGH_STATE + LEVEL_OK_STATE + PUMP_ON_STATE + TO_LOW_STATE
 
+        print(state_current)
+
         # si changement de statut
         if state_current != state:
 
