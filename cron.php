f72f4937367519377ef3f31b1efa8cd34945ff1c
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2019-09-13 19:25:18 +0200

    lecture fichier config

--- cron.php
+++ cron.php
@@ -5,14 +5,14 @@ require_once 'vendor/autoload.php';
 $strJsonFileContents = file_get_contents("config.json");
 $data = json_decode($strJsonFileContents, true);
 
-$transport = new Swift_SmtpTransport($data['gmail'][0]['server'], 465, 'ssl');
+$transport = new Swift_SmtpTransport($data['gmail'][0]['server'], $data['gmail'][0]['port_php'], 'ssl');
 $transport->setUsername($data['gmail'][0]['mail'])->setPassword($data['gmail'][0]['password']);
 
 $mailer = new Swift_Mailer($transport);
 
 $message = new Swift_Message('Test message');
 $message
-   ->setFrom([$data['gmail'][0]['mail'] => 'raspberrypi.recifal'])
+   ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
    ->setTo([$data['mail_to']])
    ->setSubject('Test message')
    ->setBody('Test Message', 'text/html');
