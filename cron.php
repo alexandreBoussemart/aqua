846a4d7be1a782052e8a77bbb9aac4d633b8b7ae
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2019-09-15 19:09:29 +0200

    cron verif

--- cron.php
+++ cron.php
@@ -2,19 +2,94 @@
 
 require_once 'vendor/autoload.php';
 
-$strJsonFileContents = file_get_contents(__DIR__ ."/config.json");
-$data = json_decode($strJsonFileContents, true);
+try {
+    $strJsonFileContents = file_get_contents(__DIR__ . "/config.json");
+    $data = json_decode($strJsonFileContents, true);
 
-$transport = new Swift_SmtpTransport($data['gmail'][0]['server'], $data['gmail'][0]['port_php'], 'ssl');
-$transport->setUsername($data['gmail'][0]['mail'])->setPassword($data['gmail'][0]['password']);
+    $transport = new Swift_SmtpTransport($data['gmail'][0]['server'], $data['gmail'][0]['port_php'], 'ssl');
+    $transport->setUsername($data['gmail'][0]['mail'])->setPassword($data['gmail'][0]['password']);
 
-$mailer = new Swift_Mailer($transport);
+    $link = new mysqli($data['database'][0]['host'], $data['database'][0]['user'], $data['database'][0]['passwd'], $data['database'][0]['database']);
+    if ($link->connect_error) {
+        $mailer = new Swift_Mailer($transport);
+        $message = new Swift_Message("Cron - ERREUR");
+        $message
+            ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
+            ->setTo([$data['mail_to']])
+            ->setSubject("Cron - ERREUR")
+            ->setBody('Connect Error (' . $link->connect_errno . ') ' . $link->connect_error, 'text/html');
 
-$message = new Swift_Message('Test message');
-$message
-   ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
-   ->setTo([$data['mail_to']])
-   ->setSubject('Test message')
-   ->setBody('Test Message', 'text/html');
+        $result = $mailer->send($message);
+    }
 
-$result = $mailer->send($message);
+    $array_verif = ['controle_bailling', 'controle_ecumeur', 'controle_osmolateur', 'controle_reacteur', 'controle_temperature'];
+    $message_body = [
+        'controle_bailling' => 'Cron - Erreur script bailling',
+        'controle_ecumeur' => 'Cron - Erreur script écumeur',
+        'controle_osmolateur' => 'Cron - Erreur script osmolateur',
+        'controle_reacteur' => 'Cron - Erreur script réacteur',
+        'controle_temperature' => 'Cron - Erreur script température'
+    ];
+
+    $periode = "-1 minute";
+
+    $date = new DateTime();
+    $today = $date->format('Y-m-d H:i:59');
+    $date->modify($periode);
+    $yesterday = $date->format('Y-m-d H:i:00');
+
+    foreach ($array_verif as $verif) {
+        $sql = "SELECT * FROM `controle` where `value` = '" . $verif . "' and  `created_at` >= '" . $yesterday . "' and `created_at` <= '" . $today . "' limit 1";
+        $controle = mysqli_query($link, $sql);
+        $row = mysqli_fetch_assoc($controle);
+
+        if ($row == NULL) {
+            $mailer = new Swift_Mailer($transport);
+
+            if (array_key_exists($verif, $message_body)) {
+                $text = strval($message_body[$verif]);
+                $body = "<p style='color:red;text-transform:uppercase;'>" . $text . "</p>";
+
+                $message = new Swift_Message($text);
+                $message
+                    ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
+                    ->setTo([$data['mail_to']])
+                    ->setSubject($text)
+                    ->setBody($body, 'text/html');
+
+                $result = $mailer->send($message);
+            }
+        }
+    }
+
+    //controle 8h
+    $date = new DateTime();
+    $current = $date->format('Y-m-d H:i:00');
+    $huit = $date->format('Y-m-d 08:00:00');
+
+    if($current == $huit){
+        $mailer = new Swift_Mailer($transport);
+        $message = new Swift_Message("Cron - contrôle 8h - OK");
+        $message
+            ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
+            ->setTo([$data['mail_to']])
+            ->setSubject("Cron - contrôle 8h - OK")
+            ->setBody("<p style='color:green;text-transform:none;'>Cron - contrôle 8h - OK</p>", 'text/html');
+
+        $result = $mailer->send($message);
+    }
+
+} catch (Exception $e) {
+    $mailer = new Swift_Mailer($transport);
+    $message = new Swift_Message("Cron - ERREUR");
+    $message
+        ->setFrom([$data['gmail'][0]['mail'] => $data['gmail'][0]['name']])
+        ->setTo([$data['mail_to']])
+        ->setSubject("Cron - ERREUR")
+        ->setBody($e->getMessage(), 'text/html');
+
+    $result = $mailer->send($message);
+}
+
+
+
