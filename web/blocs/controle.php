0179f3886cfad9dd03061b5d6d45c44483ad439d
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2019-12-16 22:34:52 +0100

    correction web accent

--- web/blocs/controle.php
+++ web/blocs/controle.php
@@ -7,7 +7,7 @@
         <div class="x_content2 log">
             <ul class="quick-list">
                 <?php foreach ($listes_controles as $controle): ?>
-                    <li><i class="fa fa-check-square-o"></i><a href="#"><strong style="text-transform: uppercase"><?= utf8_encode($controle['label']) ?> </strong>Dernière
+                    <li><i class="fa fa-check-square-o"></i><a href="#"><strong style="text-transform: uppercase"><?= $controle['label'] ?> </strong>Dernière
                             mise à jour le <strong><?= getFormattedDate($controle['created_at']) ?></strong></a></li>
                 <?php endforeach; ?>
             </ul>
