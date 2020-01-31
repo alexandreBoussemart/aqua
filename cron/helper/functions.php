99ae3c30663936f002899c120dac763b2ce56b64
Alex <aboussemart@adexos.fr>
2020-01-31 16:14:27 +0100

    Modifications qui seront validées : 	modifié :         cron/helper/functions.php 	modifié :         cron/mail.php

--- cron/helper/functions.php
+++ cron/helper/functions.php
@@ -12,10 +12,10 @@ $array_verif = [
 ];
 
 $message_body = [
-    'controle_bailling' => 'Cron - Erreur script bailling',
-    'controle_ecumeur' => 'Cron - Erreur script écumeur',
-    'controle_osmolateur' => 'Cron - Erreur script osmolateur',
-    'controle_reacteur' => 'Cron - Erreur script réacteur',
+    'controle_bailling'    => 'Cron - Erreur script bailling',
+    'controle_ecumeur'     => 'Cron - Erreur script écumeur',
+    'controle_osmolateur'  => 'Cron - Erreur script osmolateur',
+    'controle_reacteur'    => 'Cron - Erreur script réacteur',
     'controle_temperature' => 'Cron - Erreur script température'
 ];
 
@@ -116,6 +116,7 @@ function readFileTemperature($link)
 
 /**
  * @param $content
+ *
  * @return bool|float|int
  */
 function readTemperature($content)
@@ -153,7 +154,7 @@ function setControle($link, $value)
  * @param      $value
  * @param      $error
  * @param      $message
- * @param int $exclude
+ * @param int  $exclude
  * @param bool $force_log
  */
 function setState($link, $path, $value, $error, $message, $exclude = 0, $force_log = false)
@@ -199,18 +200,18 @@ function setLog($link, $message)
 function getLabel($key)
 {
     $array = [
-        '' => "",
-        'off' => "Off",
-        'ok' => "Niveau d'eau OK",
-        "pump_on" => "En cours de remplissage",
-        "to_low" => "Niveau d'eau bas",
-        "to_high" => "Niveau d'eau haut",
-        "off_rappel" => "RAPPEL - Off",
-        "to_low_rappel" => "RAPPEL - Niveau d'eau bas",
-        "pump_on_20" => "Pompe allumée plus de 20 secondes",
+        ''                  => "",
+        'off'               => "Off",
+        'ok'                => "Niveau d'eau OK",
+        "pump_on"           => "En cours de remplissage",
+        "to_low"            => "Niveau d'eau bas",
+        "to_high"           => "Niveau d'eau haut",
+        "off_rappel"        => "RAPPEL - Off",
+        "to_low_rappel"     => "RAPPEL - Niveau d'eau bas",
+        "pump_on_20"        => "Pompe allumée plus de 20 secondes",
         "pump_on_20_rappel" => "RAPPEL - Pompe allumée plus de 20 secondes",
-        "to_high_rappel" => "RAPPEL - Niveau d'eau haut",
-        "error" => "Erreur"
+        "to_high_rappel"    => "RAPPEL - Niveau d'eau haut",
+        "error"             => "Erreur"
     ];
 
     return $array[$key];
@@ -304,7 +305,9 @@ function isOn()
  * @param $data
  * @param $transport
  * @param $link
+ *
  * @return bool
+ * @throws Exception
  */
 function checkChangementEau($data, $transport, $link)
 {
@@ -395,5 +398,112 @@ function getConfig($link, $name){
         return $result;
     } catch (Exception $e) {
         setLog($link, $e->getMessage());
+    }
+}
+
+/**
+ * @param $link
+ * @param $data
+ * @param $transport
+ */
+function envoyerMail($link, $data, $transport)
+{
+    // on fait le premier mail
+    $sql = "SELECT * FROM `state` WHERE `exclude_check` LIKE 0 AND `mail_send` LIKE 0";
+    $mails = mysqli_query($link, $sql);
+    $rows = $mails->fetch_all();
+    foreach ($rows as $row) {
+        $id = $row[0];
+        $message = $row[5];
+        $error = $row[4];
+
+        if ($error == 1 || $error == "1") {
+            $body = "<p style='color: red; text-transform: uppercase'>" . $message . "</p>";
+        } else {
+            $body = "<p style='color: green;'>" . $message . "</p>";
+        }
+
+        // on envoie le mail
+        try {
+            sendMail($data, $transport, $message, $body);
+        } catch (Exception $e) {
+            setLog($link, $e->getMessage());
+        }
+
+        // on set comme quoi le mail a été envoyé et on renit la date
+        $sql = "UPDATE `state` SET `mail_send`=1,`created_at`=now() WHERE `id` LIKE " . $id;
+        $link->query($sql);
+    }
+}
+
+/**
+ * @param $link
+ * @param $data
+ * @param $transport
+ *
+ * @throws Exception
+ */
+function envoyerMailRappel($link, $data, $transport)
+{
+//on fait le mail de rappel et renit la date a now
+    $date = new DateTime();
+    $date->modify("-30 minutes");
+    $date = "'" . $date->format('Y-m-d H:i:00') . "'";
+
+    $sql = "SELECT * FROM `state` WHERE `exclude_check` LIKE 0 AND `error` LIKE 1 AND `created_at` < " . $date;
+    $mails = mysqli_query($link, $sql);
+    $rows = $mails->fetch_all();
+
+    foreach ($rows as $row) {
+        $id = $row[0];
+        $message = $row[5];
+
+        $message = str_replace('ERREUR', 'RAPPEL ERREUR', $message);
+        $body = "<p style='color: red; text-transform: uppercase'>" . $message . "</p>";
+
+        // on envoie le mail
+        try {
+            sendMail($data, $transport, $message, $body);
+        } catch (Exception $e) {
+            setLog($link, $e->getMessage());
+        }
+
+        // on renit la date
+        $sql = "UPDATE `state` SET `created_at`=now() WHERE `id` LIKE " . $id;
+        $link->query($sql);
+    }
+}
+
+/**
+ * @param $link
+ * @param $data
+ * @param $transport
+ *
+ * @throws Exception
+ */
+function envoyerMail8h($link, $data, $transport)
+{
+    //controle 8h
+    $date = new DateTime();
+    $current = $date->format('Y-m-d H:i:00');
+    $huit = $date->format('Y-m-d 08:00:00');
+
+    if ($current == $huit) {
+        $content = "<p style='color:green;text-transform:none;'>Cron - contrôle 8h - OK</p>";
+
+        $sql = "SELECT `value` FROM `data_reacteur` ORDER BY `reacteur`.`id`  DESC LIMIT 1";
+        $controle = mysqli_query($link, $sql);
+        $row = mysqli_fetch_assoc($controle);
+        $content .= "<p>Dernier débit enregistré : " . $row['value'] . " l/min</p>";
+
+        $sql = "SELECT `value` FROM `data_temperature` ORDER BY `temperature`.`id`  DESC LIMIT 1";
+        $controle = mysqli_query($link, $sql);
+        $row = mysqli_fetch_assoc($controle);
+        $content .= "<p>Dernière température enregistrée : " . round($row['value'], 2) . "°C</p>";
+
+        $message = "Cron - contrôle 8h - OK";
+
+        // on envoie le mail
+        sendMail($data, $transport, $message, $content);
     }
 }
--- cron/mail.php
+++ cron/mail.php
@@ -12,85 +12,10 @@ try {
         return false;
     }
 
-    // on fait le premier mail
-    $sql = "SELECT * FROM `state` WHERE `exclude_check` LIKE 0 AND `mail_send` LIKE 0";
-    $mails = mysqli_query($link, $sql);
-    $rows = $mails->fetch_all();
-    foreach ($rows as $row) {
-        $id = $row[0];
-        $message = $row[5];
-        $error = $row[4];
+    envoyerMail($link, $data, $transport);
+    envoyerMailRappel($link, $data, $transport);
 
-        if ($error == 1 || $error == "1") {
-            $body = "<p style='color: red; text-transform: uppercase'>" . $message . "</p>";
-        } else {
-            $body = "<p style='color: green;'>" . $message . "</p>";
-        }
-
-        // on envoie le mail
-        try {
-            sendMail($data, $transport, $message, $body);
-        } catch (Exception $e) {
-            setLog($link, $e->getMessage());
-        }
-
-        // on set comme quoi le mail a été envoyé et on renit la date
-        $sql = "UPDATE `state` SET `mail_send`=1,`created_at`=now() WHERE `id` LIKE " . $id;
-        $link->query($sql);
-    }
-
-    //on fait le mail de rappel et renit la date a now
-    $date = new DateTime();
-    $date->modify("-30 minutes");
-    $date = "'" . $date->format('Y-m-d H:i:00') . "'";
-
-    $sql = "SELECT * FROM `state` WHERE `exclude_check` LIKE 0 AND `error` LIKE 1 AND `created_at` < " . $date;
-    $mails = mysqli_query($link, $sql);
-    $rows = $mails->fetch_all();
-
-    foreach ($rows as $row) {
-        $id = $row[0];
-        $message = $row[5];
-        $error = $row[4];
-
-        $message = str_replace('ERREUR', 'RAPPEL ERREUR', $message);
-        $body = "<p style='color: red; text-transform: uppercase'>" . $message . "</p>";
-
-        // on envoie le mail
-        try {
-            sendMail($data, $transport, $message, $body);
-        } catch (Exception $e) {
-            setLog($link, $e->getMessage());
-        }
-
-        // on renit la date
-        $sql = "UPDATE `state` SET `created_at`=now() WHERE `id` LIKE " . $id;
-        $link->query($sql);
-    }
-
-    //controle 8h
-    $date = new DateTime();
-    $current = $date->format('Y-m-d H:i:00');
-    $huit = $date->format('Y-m-d 08:00:00');
-
-    if ($current == $huit) {
-        $content = "<p style='color:green;text-transform:none;'>Cron - contrôle 8h - OK</p>";
-
-        $sql = "SELECT `value` FROM `data_reacteur` ORDER BY `data_reacteur`.`id`  DESC LIMIT 1";
-        $controle = mysqli_query($link, $sql);
-        $row = mysqli_fetch_assoc($controle);
-        $content .= "<p>Dernier débit enregistré : " . $row['value'] . " l/min</p>";
-
-        $sql = "SELECT `value` FROM `data_temperature` ORDER BY `data_temperature`.`id`  DESC LIMIT 1";
-        $controle = mysqli_query($link, $sql);
-        $row = mysqli_fetch_assoc($controle);
-        $content .= "<p>Dernière température enregistrée : " . round($row['value'], 2) . "°C</p>";
-
-        $message = "Cron - contrôle 8h - OK";
-
-        // on envoie le mail
-        sendMail($data, $transport, $message, $content);
-    }
+    envoyerMail8h($link, $data, $transport);
 
 } catch (Exception $e) {
     setLog($link, $e->getMessage());
