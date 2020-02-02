e0c9b3fb304a23d475915664172db768ca74a748
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-02-02 20:29:14 +0100

    correction

--- scripts/functions.py
+++ scripts/functions.py
@@ -177,7 +177,7 @@ def setcompletestate(path, value, error, message, exclude, force_log):
         if myresult == 0:
             mydb = connect()
             mycursor = mydb.cursor()
-            sql = "UPDATE `state` set `value`='" + value + "',`error`='" + error + "',`message`=`" + message + "`, `created_at`=now(), `mail_send`=0, `exclude_check`='" + exclude + "' WHERE `path`='" + path + "'"
+            sql = 'UPDATE `state` set `value`="' + value + '",`error`="' + error + '",`message`="' + message + '", `created_at`=now(), `mail_send`=0, `exclude_check`="' + exclude + '" WHERE `path`="' + path + '"'
             mycursor.execute(sql)
             mydb.commit()
             mydb.close()
