d9dc7f0a554f8a7b765b9069003e0f8caeb41ea4
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-02-05 21:27:09 +0100

    modif controle debit reacteur

--- scripts/reacteur.py
+++ scripts/reacteur.py
@@ -25,29 +25,42 @@ GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countpulse)
 
 try:
     start_counter = 1
-    time.sleep(20)
+    time.sleep(6)
     start_counter = 0
     flow = int(round((count * 60 * 7.5 / 10)))
-    flow = int(flow / 20)
+    flow = int(flow / 6)
+
+    count = 0
+    start_counter = 1
+    time.sleep(6)
+    start_counter = 0
+    flow2 = int(round((count * 60 * 7.5 / 10)))
+    flow2 = int(flow / 6)
+
+    flow_min = flow * 0.90
+    flow_max = flow * 1.10
+
     result = False
 
-    if flow > 1050:
-        message = "Reacteur - debit reacteur OK - " + str(flow) + " l/min"
-        result = functions.setcompletestate(path, 'state_1', 0, message, 0, 0)
+    if flow_min < flow2 < flow_max:
 
-    if flow == 0:
-        message = "Reacteur - ERREUR - debit reacteur nul - 0 l/min"
-        result = functions.setcompletestate(path, 'state_2', 1, message, 0, 0)
+        if flow > 1050:
+            message = "Reacteur - debit reacteur OK - " + str(flow) + " l/min"
+            result = functions.setcompletestate(path, 'state_1', 0, message, 0, 0)
 
-    elif flow < 1050:
-        message = "Reacteur - ERREUR - debit reacteur faible - " + str(flow) + " l/min"
-        result = functions.setcompletestate(path, 'state_3', 1, message, 0, 0)
+        if flow == 0:
+            message = "Reacteur - ERREUR - debit reacteur nul - 0 l/min"
+            result = functions.setcompletestate(path, 'state_2', 1, message, 0, 0)
 
-    # si on est toutes les 15 minutes on save en bdd la valeur
-    nowMinute = datetime.datetime.now().strftime('%M')
-    minute = int(nowMinute)
-    if minute % 15 == 0 or result is True:
-        functions.setdebit(flow)
+        elif flow < 1050:
+            message = "Reacteur - ERREUR - debit reacteur faible - " + str(flow) + " l/min"
+            result = functions.setcompletestate(path, 'state_3', 1, message, 0, 0)
+
+        # si on est toutes les 15 minutes on save en bdd la valeur
+        nowMinute = datetime.datetime.now().strftime('%M')
+        minute = int(nowMinute)
+        if minute % 15 == 0 or result is True:
+            functions.setdebit(flow)
 
     functions.setcontrole('controle_reacteur')
     sys.exit()
