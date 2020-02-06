acc2b761a5b85c8dcaa5d59901cb9b0b09ef22ae
Alex <aboussemart@adexos.fr>
2020-02-06 15:08:51 +0100

    changement design

--- web/blocs/controle.php
+++ web/blocs/controle.php
@@ -61,12 +61,14 @@
                     </div>
                 </div>
 
+                <div class="ln_solid"></div>
+
                 <div class="form-group">
                     <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                         <button type="submit" class="btn btn-success">Enregistrer</button>
                     </div>
                 </div>
-                <div class="ln_solid"></div>
+
             </form>
         </div>
     </div>
