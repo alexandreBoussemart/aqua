5ac0eaaf35551ff4129d8d426450d6e884355a34
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-04-02 19:56:50 +0200

    changement url menu

--- web/blocs/menu.php
+++ web/blocs/menu.php
@@ -17,8 +17,8 @@
                     <li><a><i class="fa fa-home"></i> Accueil <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="<?= $data['database'][0]['base_url']; ?>">Dashboard</a></li>
-                            <li><a href="logs.php">Log</a></li>
-                            <li><a href="schema.php">Schéma de branchement</a></li>
+                            <li><a href="<?= $data['database'][0]['base_url']; ?>logs.php">Log</a></li>
+                            <li><a href="<?= $data['database'][0]['base_url']; ?>schema.php">Schéma de branchement</a></li>
                         </ul>
                     </li>
                 </ul>
