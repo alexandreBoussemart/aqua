187f24fa3366e68321d75ab7d551b4443fc11727
Alex <aboussemart@adexos.fr>
2020-04-03 18:09:11 +0200

    page configuration

--- web/blocs/menu.php
+++ web/blocs/menu.php
@@ -17,9 +17,9 @@
                     <li><a><i class="fa fa-home"></i> Accueil <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="<?= $data['database'][0]['base_url']; ?>">Dashboard</a></li>
-                            <li><a href="<?= $data['database'][0]['base_url']; ?>logs.php">Log</a></li>
                             <li><a href="<?= $data['database'][0]['base_url']; ?>configuration.php">Configuration</a></li>
                             <li><a href="<?= $data['database'][0]['base_url']; ?>schema.php">Schéma de branchement</a></li>
+                            <li><a href="<?= $data['database'][0]['base_url']; ?>logs.php">Log</a></li>
                         </ul>
                     </li>
                 </ul>
