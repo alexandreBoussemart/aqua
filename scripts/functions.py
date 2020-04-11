8e4cecbb9c437e09ff67a718b6e79aee14ad26a8
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-04-11 18:59:04 +0200

    test file existe

--- scripts/functions.py
+++ scripts/functions.py
@@ -7,6 +7,8 @@ import RPi.GPIO as GPIO
 import smtplib
 import mysql.connector
 import os
+import os.path
+from os import path
 
 
 def connect():
@@ -201,15 +203,7 @@ def setcompletestate(path, value, error, message, exclude, force_log):
         currentDir = os.path.dirname(os.path.realpath(__file__))
         file = currentDir + '/../state/' + path + '-' + value
 
-        # on vérifie qu'on est pas déjà dans cet état
-        mydb = connect()
-        mycursor = mydb.cursor()
-        mycursor.execute(
-            "SELECT count(*) as count FROM `state` WHERE `path` = '" + path + "' AND `value` = '" + value + "'")
-        myresult = mycursor.fetchone()[0]
-        mydb.close()
-
-        if myresult == 0:
+        if path.exists(file):
             mydb = connect()
             mycursor = mydb.cursor()
             sql = 'UPDATE `state` set `value`="' + value + '",`error`="' + error + '",`message`="' + message + '", `created_at`=now(), `mail_send`=0, `exclude_check`="' + exclude + '" WHERE `path`="' + path + '"'
