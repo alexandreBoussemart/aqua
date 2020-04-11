5ba2f3fa1cd5ab33715bd6321a1938c84a81521a
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-04-11 19:05:35 +0200

    test file existe

--- scripts/functions.py
+++ scripts/functions.py
@@ -202,7 +202,7 @@ def setcompletestate(path, value, error, message, exclude, force_log):
         currentDir = os.path.dirname(os.path.realpath(__file__))
         file = currentDir + '/../state/' + path + '-' + value
 
-        if os.path.exists(file):
+        if os.path.isfile(file):
             mydb = connect()
             mycursor = mydb.cursor()
             sql = 'UPDATE `state` set `value`="' + value + '",`error`="' + error + '",`message`="' + message + '", `created_at`=now(), `mail_send`=0, `exclude_check`="' + exclude + '" WHERE `path`="' + path + '"'
