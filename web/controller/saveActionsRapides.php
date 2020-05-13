448ca8fea822cecd23e077390c3f6a0e6ab0e738
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-05-14 01:46:25 +0200

    perf

--- web/controller/saveActionsRapides.php
+++ web/controller/saveActionsRapides.php
@@ -3,6 +3,6 @@
 require __DIR__ . '/../../app/code/Helper/functions.php';
 
 // form actions rapides
-setStatus($link, $_POST['on_off_osmolateur'], 'on_off_osmolateur');
-setStatus($link, $_POST['on_off_ecumeur'], 'on_off_ecumeur');
+setStatus($link, isset($_POST['on_off_osmolateur']) ? $_POST['on_off_osmolateur'] : null, 'on_off_osmolateur');
+setStatus($link, isset($_POST['on_off_ecumeur']) ? $_POST['on_off_ecumeur'] : null, 'on_off_ecumeur');
 
