9676a9d6cacf6981d45ae2c6fc22be6005faf1c4
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2020-06-12 13:48:11 +0200

    revert

--- app/code/templates/head.php
+++ app/code/templates/head.php
@@ -5,4 +5,3 @@
 <link rel="icon" href="src/images/favicon.ico" type="./src/image/ico"/>
 <link href="./build/css/library.min.css" rel="stylesheet">
 <link href="./build/css/custom.min.css" rel="stylesheet">
-<meta name = "viewport" content = "width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no">
