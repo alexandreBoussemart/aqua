5e41475ee47d90f8c4e0f4ef171fa22a4dbc81be
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2021-01-19 21:49:43 +0100

    modif osmolateur python

--- scripts/osmolateur.py
+++ scripts/osmolateur.py
@@ -10,7 +10,6 @@ import RPi.GPIO as GPIO
 relais = 21
 to_high = 23
 level_ok = 24
-pump_on = 25
 to_low = 27
 
 path = 'osmolateur'
@@ -21,7 +20,6 @@ GPIO.setwarnings(False)
 try:
     GPIO.setup(to_high, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(to_low, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(pump_on, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(level_ok, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # lecture GPIO led to high
@@ -30,21 +28,17 @@ try:
     # lecture GPIO led to low
     TO_LOW_STATE = str(GPIO.input(to_low))
 
-    # lecture GPIO led pump on
-    PUMP_ON_STATE = str(GPIO.input(pump_on))
-
     # lecture GPIO led level ok
     LEVEL_OK_STATE = str(GPIO.input(level_ok))
 
     # on fait la chaine de caract�re du statut
-    state = TO_HIGH_STATE + LEVEL_OK_STATE + PUMP_ON_STATE + TO_LOW_STATE
+    state = TO_HIGH_STATE + LEVEL_OK_STATE + TO_LOW_STATE
 
     time.sleep(0.5)
 
     # on fait une deuxi�me lecture pour �tre s�r
     GPIO.setup(to_high, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(to_low, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(pump_on, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(level_ok, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # lecture GPIO led to high
@@ -53,44 +47,41 @@ try:
     # lecture GPIO led to low
     TO_LOW_STATE = str(GPIO.input(to_low))
 
-    # lecture GPIO led pump on
-    PUMP_ON_STATE = str(GPIO.input(pump_on))
-
     # lecture GPIO led level ok
     LEVEL_OK_STATE = str(GPIO.input(level_ok))
 
     # on fait la chaine de caract�re du statut
-    state2 = TO_HIGH_STATE + LEVEL_OK_STATE + PUMP_ON_STATE + TO_LOW_STATE
+    state2 = TO_HIGH_STATE + LEVEL_OK_STATE + TO_LOW_STATE
 
     # si les deux mesures sont identiques
     if state == state2:
+        # niveau d'eau TO LOW
+        if TO_LOW_STATE == '1':
+            functions.setosmolateur("to_low")
+            message = "Osmolateur - ERREUR - niveau eau TO LOW"
+            functions.setcompletestate(path, 'state_4', 1, message, 1, 0)
+
         # niveau d'eau TO HIGHT
-        if state == '0111':
+        elif TO_HIGH_STATE == '1':
             functions.setosmolateur("to_high")
             message = "Osmolateur - ERREUR - niveau eau TO HIGHT"
             functions.setcompletestate(path, 'state_1', 1, message, 0, 0)
 
         # niveau d'eau OK
-        elif state == '1011':
+        elif LEVEL_OK_STATE == '1':
             functions.setosmolateur("ok")
             message = "Osmolateur - niveau eau OK"
             functions.setcompletestate(path, 'state_2', 0, message, 0, 0)
 
-        # remplissage en cours
-        elif state == '1101':
+        # niveau d'eau OK
+        elif LEVEL_OK_STATE == '0':
             functions.setosmolateur("pump_on")
             message = "Osmolateur - remplissage en cours"
             if functions.notinstatehuit() is True:
                 functions.setcompletestate(path, 'state_3', 0, message, 0, 0)
 
-        # niveau d'eau TO LOW
-        elif state == '1110':
-            functions.setosmolateur("to_low")
-            message = "Osmolateur - ERREUR - niveau eau TO LOW"
-            functions.setcompletestate(path, 'state_4', 1, message, 1, 0)
-
         # osmolateur off
-        elif state == '1111':
+        else:
             functions.setosmolateur("off")
             message = "Osmolateur - ERREUR - off"
             functions.setcompletestate(path, 'state_5', 1, message, 1, 0)
