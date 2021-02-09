ac852ea853c5060ec95931dcf94a4c47da81fe61
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2021-02-09 19:10:54 +0100

    suppression capteur bas + btn simulate remplissage + update schema branchement + rename dossier cron

--- app/code/templates/schema/schema.php
+++ app/code/templates/schema/schema.php
@@ -1,23 +1,3 @@
-<?php
-// logs
-$sql = "# noinspection SqlNoDataSourceInspectionForFile  
-        SELECT * 
-        FROM ".TABLE_LOG." 
-        ORDER BY `id` DESC 
-        LIMIT 30;";
-$request = mysqli_query($link, $sql);
-$logs = mysqli_query($link, $sql);
-
-// changement eau
-$sql = "# noinspection SqlNoDataSourceInspectionForFile  
-        SELECT * 
-        FROM ".TABLE_DATA_CHANGEMENT_EAU." 
-        ORDER BY `id` DESC 
-        LIMIT 30;";
-$request = mysqli_query($link, $sql);
-$changements = mysqli_query($link, $sql);
-?>
-
 <div class="row first-bloc">
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
@@ -28,24 +8,19 @@ $changements = mysqli_query($link, $sql);
             <div class="x_content2">
                 <table class="schema">
                     <tr>
-                        <td>Capteur<br> osmolateur<br> réserve</td>
-                        <td>Bouton<br> poussoir<br> Osmolateur</td>
-                        <td>Interrupteur<br> pompe<br> Osmolateur</td>
-                    </tr>
-                    <tr>
                         <td>Pompe<br> Osmolateur</td>
                         <td>Capteur<br> Osmolateur</td>
-                        <td>Ventilateur<br> Aquarium</td>
+                        <td>Interrupteur<br> pompe<br> Osmolateur</td>
                     </tr>
                     <tr>
                         <td>Capteur<br> Bailling</td>
                         <td>Capteur<br> Température</td>
-                        <td>Capteur<br> Écumeur</td>
+                        <td>Ventilateur<br> Aquarium</td>
                     </tr>
                     <tr>
                         <td>Réacteur<br> Éclairage<br> Ventilateur</td>
                         <td>Capteur<br> Réacteur</td>
-                        <td></td>
+                        <td>Capteur<br> Écumeur</td>
                     </tr>
                 </table>
             </div>
--- scripts/osmolateur.py
+++ scripts/osmolateur.py
@@ -10,7 +10,6 @@ import RPi.GPIO as GPIO
 relay = 21
 to_high = 23
 level_ok = 24
-to_low = 27
 
 path = 'osmolateur'
 
@@ -19,39 +18,31 @@ GPIO.setwarnings(False)
 
 try:
     GPIO.setup(to_high, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(to_low, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(level_ok, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # lecture GPIO led to high
     TO_HIGH_STATE = str(GPIO.input(to_high))
 
-    # lecture GPIO led to low
-    TO_LOW_STATE = str(GPIO.input(to_low))
-
     # lecture GPIO led level ok
     LEVEL_OK_STATE = str(GPIO.input(level_ok))
 
     # on fait la chaine de caract�re du statut
-    state = TO_HIGH_STATE + LEVEL_OK_STATE + TO_LOW_STATE
+    state = TO_HIGH_STATE + LEVEL_OK_STATE
 
     time.sleep(0.5)
 
     # on fait une deuxi�me lecture pour �tre s�r
     GPIO.setup(to_high, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(to_low, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(level_ok, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # lecture GPIO led to high
     TO_HIGH_STATE = str(GPIO.input(to_high))
 
-    # lecture GPIO led to low
-    TO_LOW_STATE = str(GPIO.input(to_low))
-
     # lecture GPIO led level ok
     LEVEL_OK_STATE = str(GPIO.input(level_ok))
 
     # on fait la chaine de caract�re du statut
-    state2 = TO_HIGH_STATE + LEVEL_OK_STATE + TO_LOW_STATE
+    state2 = TO_HIGH_STATE + LEVEL_OK_STATE
 
     # si les deux mesures sont identiques
     if state == state2:
@@ -62,12 +53,6 @@ try:
             functions.setosmolateur("off")
             message = "Osmolateur - off"
             functions.setcompletestate(path, 'state_5', 1, message, 1, 0)
-
-        # niveau d'eau TO LOW
-        elif TO_LOW_STATE == '1':
-            functions.setosmolateur("to_low")
-            message = "Osmolateur - ERREUR - niveau eau TO LOW"
-            functions.setcompletestate(path, 'state_4', 1, message, 1, 0)
 
         # niveau d'eau TO HIGHT
         elif TO_HIGH_STATE == '1':
