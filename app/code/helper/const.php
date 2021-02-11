2c68efcd4b5a516ed69daf5a6bd1edbbeaa324e6
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2021-02-11 19:17:56 +0100

    rename table

--- app/code/helper/const.php
+++ app/code/helper/const.php
@@ -14,18 +14,19 @@ const REACTEUR = 'reacteur';
 
 /* tables */
 const TABLE_CORE_CONFIG = '`core_config_value`';
-const TABLE_STATE = '`state`';
 const TABLE_STATUS = '`core_config_status`';
-const TABLE_LOG_MAIL = '`log_mail`';
-const TABLE_DATA_EAU = '`data_parametres_eau`';
+const TABLE_STATE = '`core_config_state`';
 const TABLE_DATA_CHANGEMENT_EAU = '`data_changement_eau`';
-const TABLE_LOG = '`log`';
-const TABLE_LAST_ACTIVITY = '`last_activity`';
-const TABLE_DATA_REACTEUR = '`data_reacteur`';
-const TABLE_DATA_TEMP_EAU = '`data_temperature_eau`';
-const TABLE_DATA_TEMP_AIR = '`data_temperature_air`';
-const TABLE_DATA_TEMP_RPI = '`data_temperature_rpi`';
 const TABLE_DATA_DEPENSE = '`data_depense`';
+const TABLE_DATA_EAU = '`data_parametres_eau`';
+const TABLE_DATA_REACTEUR = '`data_reacteur`';
+const TABLE_DATA_TEMP_AIR = '`data_temperature_air`';
+const TABLE_DATA_TEMP_EAU = '`data_temperature_eau`';
+const TABLE_DATA_TEMP_RPI = '`data_temperature_rpi`';
+
+const TABLE_LAST_ACTIVITY = '`last_activity`';
 const TABLE_TIMER = '`timer`';
+const TABLE_LOG = '`log`';
+const TABLE_LOG_MAIL = '`log_mail`';
 
 const DISABLE_ALL_CRON = 'disable_all_cron';
