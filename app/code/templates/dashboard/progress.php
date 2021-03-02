500ada9aea59bdb9aedcd265a8b449bad1e381f6
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2021-03-02 20:18:46 +0100

    add temps restant progress

--- app/code/templates/dashboard/progress.php
+++ app/code/templates/dashboard/progress.php
@@ -7,8 +7,12 @@
         <div class="x_content progress-content">
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'ca') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test Ca <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test Ca <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -17,8 +21,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'mg') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test Mg <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test Mg <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -27,8 +35,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'kh') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test Kh <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test Kh <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -37,8 +49,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'densite') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test densité <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test densité <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -47,8 +63,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'nitrate') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test nitrate <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test nitrate <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -57,8 +77,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_parametres_eau', $type = 'phosphate') ?>
             <?php $numberMax = getConfig($link, 'check_analyse_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Test phosphate <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Test phosphate <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-warning"
                      role="progressbar"
@@ -67,8 +91,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_changement_eau') ?>
             <?php $numberMax = getConfig($link, 'check_changement_eau'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Changement d'eau <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Changement d'eau <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped"
                      role="progressbar"
@@ -77,8 +105,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_clean_reacteur') ?>
             <?php $numberMax = getConfig($link, 'check_clean_reacteur'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Nettoyage réacteur <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Nettoyage réacteur <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-success"
                      role="progressbar"
@@ -87,8 +119,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_clean_ecumeur') ?>
             <?php $numberMax = getConfig($link, 'check_clean_ecumeur'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Nettoyage écumeur <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Nettoyage écumeur <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-success"
                      role="progressbar"
@@ -97,8 +133,12 @@
             </div>
             <?php $numberNow = getDaysProgess($link, 'data_clean_pompes') ?>
             <?php $numberMax = getConfig($link, 'check_clean_pompes'); ?>
+            <?php $tempsRestant = max(0, $numberMax - $numberNow); ?>
             <?php $transitiongoal = getTransitiongoal($numberNow, $numberMax) ?>
-            <p>Nettoyages pompes <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small></p>
+            <p>
+                Nettoyages pompes <small>(<?= $numberNow; ?>/<?= $numberMax; ?>J)</small>
+                <span><small>Temps restant : <?= $tempsRestant ?>J</small></span>
+            </p>
             <div class="progress active">
                 <div class="progress-bar progress-bar-striped progress-bar-success"
                      role="progressbar"
