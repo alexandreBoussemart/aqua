b9ca045c3d7aea583ad3fc9482481b042dd32663
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2021-03-09 22:05:58 +0100

    correction mobile

--- app/code/templates/budget/form.php
+++ app/code/templates/budget/form.php
@@ -49,7 +49,7 @@
                 </div>
                 <div class="ln_solid"></div>
                 <div class="form-group">
-                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3 text-center">
+                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                         <button type="submit" class="btn btn-success">
                             <i class="fa fa-save"></i> Enregistrer
                         </button>
